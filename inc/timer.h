917aafab70d946663cbf369af8eea4d16a03807f
Kayke Vertu <vertu_kayke@hotmail.com>
2024-09-28 18:59:13 -0300

    Implementação do temporizador

---
+++ inc/timer.h
@@ -0,0 +1,61 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <time.h>
+#include <stdbool.h>
+
+/*
+    Váriaveis globais para controle do temporizador
+*/
+
+clock_t current;
+clock_t elapsed = 0;
+bool paused = true;
+
+/*
+    Reinicia o temporizador
+*/
+void reset()
+{
+    elapsed = 0;
+    paused = true;
+}
+
+/*
+    Inicia/retoma o temporizador, caso esteja pausado
+*/
+void start()
+{
+    if (paused)
+    {
+        current = clock();
+        paused = false;
+    }
+}
+
+/*
+    Para o temporizador, caso não esteja pausado
+*/
+void stop()
+{
+    if (!paused)
+    {
+        elapsed = clock() - current + elapsed;
+        paused = true;
+    }
+}
+
+/*
+    Retorna o tempo marcado em clocks
+*/
+clock_t getElapsedTimeInClocks()
+{
+    return paused ? elapsed : clock() - current + elapsed;
+}
+
+double getElapsedTimeInSeconds()
+{
+    return (double) getElapsedTimeInClocks() / CLOCKS_PER_SEC;
+}
+
+#endif
--- test2.c
+++ test2.c
@@ -1,7 +1,8 @@
 #include "inc/base.h"
 #include "inc/sort.h"
+#include "inc/timer.h"
 
-#define ARRAY_SIZE 300000
+#define ARRAY_SIZE 100000
 
 int main()
 {
@@ -11,9 +12,17 @@ int main()
     identity(test, ARRAY_SIZE);
     copy(copied, test, ARRAY_SIZE);
     shuffle(copied, ARRAY_SIZE);
-    print(copied, ARRAY_SIZE);
+    // print(copied, ARRAY_SIZE);
+
+    reset();
+    printf("Começou!\n");
+    start();
     insertionSort(copied, ARRAY_SIZE);
-    print(copied, ARRAY_SIZE);
+    stop();
+    printf("Acabou!\n");
+
+    // print(copied, ARRAY_SIZE);
+    printf("\nTempo de execução: %lfs (%ld clocks)\n", getElapsedTimeInSeconds(), getElapsedTimeInClocks());
 
     return 0;
 }
